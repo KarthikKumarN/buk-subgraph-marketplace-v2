1ba058aa38e517a9ee8afae196e9e163de6dc02c
karthik-buk <karthikn503@gmail.com>
2024-06-28 20:23:54 +0530

    Handling Room bought💶

--- src/marketplace.ts
+++ src/marketplace.ts
@@ -206,17 +206,55 @@ export function handleDeletedListing(event: DeletedListingEvent): void {
 }
 
 export function handleRoomBought(event: RoomBoughtEvent): void {
-	let entity = new RoomBought(
+	// Create a new Booking entity using the transaction hash and log index
+	let entity = new Booking(
 		event.transaction.hash.concatI32(event.logIndex.toI32())
 	);
-	entity.tokenId = event.params.tokenId;
-	entity.previousOwner = event.params.previousOwner;
-	entity.newOwner = event.params.newOwner;
-	entity.price = event.params.price;
 
-	entity.blockNumber = event.block.number;
-	entity.blockTimestamp = event.block.timestamp;
-	entity.transactionHash = event.transaction.hash;
+	// Bind the BukProtocol contract to an instance
+	let bukProtocol = BukProtocol.bind(Address.fromString(BUK_PROTOCOL));
 
-	entity.save();
+	// Bind the BukNFTs contract to an instance
+	let bukNfts = BukNFTs.bind(Address.fromString(BUK_NFTs));
+
+	// Bind the Marketplace contract to an instance
+	let bukTrips = Marketplace.bind(Address.fromString(BUK_MARKETPLACE));
+
+	// Get the tokenId from the event parameters
+	const tokenId = event.params.tokenId;
+
+	// Try to get the booking details for the given tokenId
+	let booking = bukProtocol.try_getBookingDetails(tokenId);
+
+	// Try to get the booking details for the given tokenId
+	let listing = bukTrips.try_getListingDetails(tokenId);
+
+	// Check if the booking details were not reverted
+	if (!booking.reverted) {
+		// Convert the propertyId from bytes to string
+		const propertyId = bytesToString(booking.value.propertyId);
+
+		// Set the properties of the Booking entity
+		entity.id = event.transaction.hash.concatI32(event.logIndex.toI32());
+		entity.nftId = tokenId;
+		entity.propertyId = propertyId;
+		entity.adult = booking.value.adult;
+		entity.child = booking.value.child;
+		entity.checkIn = booking.value.checkin;
+		entity.checkOut = booking.value.checkout;
+		entity.owner = Bytes.fromHexString(DEFAULT_ADDRESS);
+		entity.price = BigInt.fromI32(0);
+		entity.index = listing.value.index;
+		entity.transactionHash = event.transaction.hash;
+
+		// Try to get the IPFS URI for the tokenId
+		const ipfsUri = bukNfts.try_uri(tokenId);
+		if (!ipfsUri.reverted) {
+			// Set the IPFS URI for the Booking entity if it was not reverted
+			entity.ipfsUri = ipfsUri.value;
+		}
+
+		// Save the Booking entity to the datastore
+		entity.save();
+	}
 }
